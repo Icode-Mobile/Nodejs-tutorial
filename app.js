2efab4e42777080539bdc73480d059c0e3197bbe
Caio18-cosenza <caiiocosenza2020@gmail.com>
2021-09-11 20:12:27 -0300

    Segunda aula NodeJS

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const port = process.env.PORT || 80;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    erro: false,
+    message: 'Rota GET funcionando!',
+  });
+});
+
 app.listen(port, (req, res) => {
   console.log(`Servidor Rodando! Url: http://localhost:${port}`);
 });
